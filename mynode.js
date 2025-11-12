c992c00da387e1d3c9ac48e4a705cf50eda5a4dd
Nicolas <nico.daddabbo7100@gmail.com>
2025-11-12 17:19:22 +0100

    teste

--- mynode.js
+++ mynode.js
@@ -16,10 +16,7 @@ const envFile = `export const environment = {
 },
 };
 `;
-const targetPath = path.join(
-  __dirname,
-  "./src/environments/environment.development.ts"
-);
+const targetPath = path.join(__dirname, "./src/environments/environment.ts");
 fs.writeFile(targetPath, envFile, (err) => {
   if (err) {
     console.error(err);
@@ -27,7 +24,7 @@ fs.writeFile(targetPath, envFile, (err) => {
   } else {
     console.log(
       successColor,
-      `${checkSign} Successfully generated environment.development.ts`
+      `${checkSign} Successfully generated environment.ts`
     );
   }
 });
